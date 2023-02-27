38f1f2cd8bc06500a796d1ab2f07ccbc2d2f691f
SirPudwhip <spamfolder3399@gmail.com>
2023-02-27 13:11:44 -0500

    code along done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+
 
 function Toggle() {
-  return <button>OFF</button>;
+
+const [isOn, setIsOn] = useState(true);
+
+const handleClick = () => {setIsOn(isOn => !isOn)};
+
+  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
+
+// function Toggle() {
+//   const [isOn, setIsOn] = useState(false);
+
+//   function handleClick() {
+//     setIsOn((isOn) => !isOn);
+//   }
+
+//   return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+// }
 
 export default Toggle;
